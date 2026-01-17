badd96a2f94828e7eab97cebd4c70077f9c23e6f
Alanzoka <zokareacts@gmail.com>
2026-01-17 06:03:35 +0000

    Por que ta dando erro

--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-// IMPORTANT: Populate the .env.local file with your actual Firebase project configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyCJ1h9t9lEyvXt2BkzqgPHoointLSW6Kps",
-  authDomain: "studio-9846334710-a80ac.firebaseapp.com",
-  projectId: "studio-9846334710-a80ac",
-  storageBucket: "studio-9846334710-a80ac.appspot.com",
-  messagingSenderId: "638334388858",
-  appId: "1:638334388858:web:d7eac714fc1a7a81d149c3",
-};
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,7 +1,15 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { firebaseConfig } from './config';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ1h9t9lEyvXt2BkzqgPHoointLSW6Kps",
+  authDomain: "studio-9846334710-a80ac.firebaseapp.com",
+  projectId: "studio-9846334710-a80ac",
+  storageBucket: "studio-9846334710-a80ac.appspot.com",
+  messagingSenderId: "638334388858",
+  appId: "1:638334388858:web:d7eac714fc1a7a81d149c3",
+};
 
 export function initializeFirebase() {
     const firebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
