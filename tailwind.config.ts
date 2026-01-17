d58c1db0c938534af61b22b27afbf156bcc0fa37
Alanzoka <zokareacts@gmail.com>
2026-01-17 01:07:11 +0000

    Use a font sora no site geral

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['"Space Grotesk"', 'sans-serif'],
-        headline: ['"Space Grotesk"', 'sans-serif'],
+        body: ['"Sora"', 'sans-serif'],
+        headline: ['"Sora"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
